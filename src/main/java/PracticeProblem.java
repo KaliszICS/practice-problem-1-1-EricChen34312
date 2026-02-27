dfc1cf3315d60522365b3f76e9678c7919065d60
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-02-27 16:07:55 +0000

    Solve Q1-Q3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -24,4 +24,43 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	//the real start of the answers
+
+	public static int sum2D(int[][] arr){
+		//initiate counter
+		int out = 0;
+
+		//iterate through rows 
+		for(int i = 0; i < arr.length; i++){
+
+			//add every value in row i to counter
+			for(int j = 0; j < arr[i].length; j++){
+				out += arr[i][j];
+			}
+		}
+		return out;
+	}
+
+	public static int sumRow(int[][] arr, int row){
+		int out = 0;
+
+		//add every value in arr[row] to a counter and return counter
+		for(int i = 0; i < arr[row].length; i++){
+			out += arr[row][i];
+		}
+
+		return out;
+	}
+
+	public static int sumColumn(int[][] arr, int col){
+		int out = 0;
+
+		//add every value in arr[][col] to counter, return counter
+		for(int i = 0; i < arr.length; i++){
+			out += arr[i][col];
+		}
+
+		return out;
+	}
+
 }
